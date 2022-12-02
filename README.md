f61c79ca7d0ef3a165d35981826f9b9cd0154aa1
Henil Panchal <henilp105@gmail.com>
2022-12-03 01:20:53 +0530

    docker flask app

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # registry
 Registry for Fortran package manager
+
+## Python Flask app with Nginx and  Mongo database
+
+Project structure:
+```
+.
+├── compose.yaml
+├── flask
+│   ├── Dockerfile
+│   ├── requirements.txt
+│   └── server.py
+└── nginx
+    └── nginx.conf
+
+```
+
+## Deploy with docker compose
+
+```
+$ sudo chmod 666 /var/run/docker.sock  (for root access)
+$ docker compose up -d
+
+After the application starts, navigate to `http://localhost:80` in your web browser or run:
+
+```
+$ curl localhost:80
+Hello world, Mongo Flask
+```
+
+Stop and remove the containers
+```
+$ docker compose down
+```
---
+++ flask/server.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+import os
+from flask import Flask
+from flask import render_template
+from flask import flash, request
+from pymongo import MongoClient
+import itertools
+from datetime import datetime
+import json
+
+app = Flask(__name__)
+
+client = MongoClient("mongo:27017")
+db = client.fpmregistry
+
+@app.route('/')
+def index():
+    try:
+        client.admin.command('ismaster')
+    except:
+        return "Server not available"
+    return "Hello world, Mongo Flask"
+
+@app.route('/login', methods=['GET'])
+def render_main():
+   return render_template("login.html")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT", 9090), debug=True)
+
