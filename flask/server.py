74d114bc5c3db5de04b94967623d7630df14f241
Henil Panchal <henilp105@gmail.com>
2022-12-23 22:32:37 +0530

    user crud

--- flask/server.py
+++ flask/server.py
@@ -1,30 +1,127 @@
-#!/usr/bin/env python
 import os
 from flask import Flask
 from flask import render_template
-from flask import flash, request
-from pymongo import MongoClient
-import itertools
+from flask import request, make_response, jsonify
 from datetime import datetime
-import json
+from pymongo import MongoClient   
+import re
+from uuid import uuid4
 
 app = Flask(__name__)
 
-client = MongoClient("mongo:27017")
-db = client.fpmregistry
+client = MongoClient("mongo:27017") 
+db = client.fpmregistry 
 
 @app.route('/')
 def index():
-    try:
-        client.admin.command('ismaster')
-    except:
-        return "Server not available"
-    return "Hello world, Mongo Flask"
+    return render_template("login.html")
 
-@app.route('/login', methods=['GET'])
-def render_main():
-   return render_template('login.html')
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        uuid = request.cookies.get("uuid")
+        if not uuid:
+            email=request.form.get('email')
+            password=request.form.get('password')
+            user = db.users.find_one({"email": email, "password": password})
+        else:
+            user = db.users.find_one({"uuid": uuid})
+
+        if not user:
+            return "Invalid email or password", 401
+
+        db.users.update_one({"_id": user["_id"]}, {"$set": {"loginAt": datetime.utcnow()}})
+
+        uuid = uuid4().hex
+
+        db.users.update_one({"_id": user["_id"]}, {"$set": {"uuid": uuid}})
+
+        response = make_response("Login successful")
+        response.set_cookie("uuid", uuid)
+        return response
+    else:
+        return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == 'POST':
+        uuid = request.cookies.get("uuid")
+        if not uuid:
+            name=request.form.get('name')
+            email=request.form.get('email')
+            password=request.form.get('password')
+            user = db.users.find_one({"email": email})
+        else:
+            user = db.users.find_one({"uuid": uuid})
+
+        if user:
+            return "A user with this email already exists", 400
+
+        user = {
+            "name": name,
+            "email": email,
+            "password": password,
+            "lastLogin": datetime.utcnow(),
+            "createdAt": datetime.utcnow(),
+            "uuid":"",
+        }
+        db.users.insert_one(user)
+        uuid = uuid4().hex
+        db.users.update_one({"_id": user["_id"]}, {"$set": {"uuid": uuid}})
+
+        response = make_response("Signup successful")
+        response.set_cookie("uuid", uuid)
+        return response
+    else:
+        return render_template("login.html")
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    uuid = request.cookies.get("uuid")
+    if not uuid:
+        return jsonify({"message": "UUID not found", "code": 404})
+
+    user = db.users.find_one({"uuid": uuid})
+    if not user:
+        return jsonify({"message": "User not found", "code": 404})
+
+    db.users.update_one({"_id": user["_id"]}, {"$set": {"lastLogout": datetime.utcnow()}})
+
+    db.users.update_one({"_id": user["_id"]}, {"$set": {"uuid": ""}})
+
+    response = make_response("Logout successful")
+    response.set_cookie("uuid", "", expires=0)
+    return response
+
+@app.route("/forgotpassword/", methods=["POST"])
+def forgotpassword():
+    password=request.form.get('password')
+    uuid=request.form.get('uuid')
+    user = db.users.find_one({"uuid": uuid})
+    if not user:
+        return jsonify({"message": "User not found", "code": 404})
+
+    db.users.update_one({"_id": user["_id"]}, {"$set": {"uuid": ""}})
+    db.users.update_one({"_id": user["_id"]}, {"$set": {"password": password}})
+    response = make_response("Password reset successful")
+    response.set_cookie("uuid", "", expires=0)
+    return response
+
+@app.route("/reset-password", methods=["POST"])
+def reset_password():
+    if request.method == 'POST':
+        email = request.form["email"]
+        user = db.users.find_one({"email": email})
+        if not user:
+            return jsonify({"message": "User not found", "code": 404})
+
+        uuid = uuid4().hex
+
+        db.users.update_one({"_id": user["_id"]}, {"$set": {"uuid": uuid}})
+
+        # send the link in the email
 
 @app.errorhandler(404)
 def page_not_found(e):
