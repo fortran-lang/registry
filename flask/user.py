615183e0b90a9c694b783a30c9a6cdad7fd9f503
arteevraina <arteevraina@gmail.com>
2023-01-14 18:45:49 +0530

    refactor: remove api from user module

--- flask/user.py
+++ flask/user.py
@@ -28,31 +28,3 @@ def profile(username):
     #     "profile.html", user=user, packages=[package for package in packages]
     # )
 
-@app.route("/users/<username>/<package_name>/delete", methods=["GET", "POST"])
-def delete_package(username, package_name):
-    uuid = request.cookies.get("uuid")
-    if not uuid:
-        return render_template("login.html")
-
-    user = db.users.find_one({"uuid": uuid})
-
-    # If user is null or username does not matches with the username in the route.
-    if not user or user["name"] != username:
-        return render_template("login.html")
-
-    package = db.packages.find_one({"name": package_name})
-
-    # If package is not found related to the package_id. Return 404.
-    if not package:
-        return jsonify({"message": "Package not found", "code": 404})
-
-    # Check if the user is authorized to delete the package.
-    if user["_id"] != package["author"] and user["_id"] not in package["maintainers"]:
-        return jsonify({"message": "User is not authorized to delete the package", "code": 401})
-
-    package_deleted = db.packages.delete_one({"name": package_name})
-
-    if package_deleted.deleted_count == 1:
-        return jsonify({"message": "Package deleted successfully", "code": 200})
-    else:
-        return jsonify({"message": "Package not found", "code": 400})
