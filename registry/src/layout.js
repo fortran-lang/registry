94b7597ca03f232c45473044d53903ff1a26fb57
Henil Panchal <henilp105@gmail.com>
2023-01-21 22:29:58 +0530

    navbar

--- registry/src/layout.js
+++ registry/src/layout.js
@@ -1,46 +1,6 @@
 import { Outlet, Link } from "react-router-dom";
 import "./layout.css";
 
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import NavDropdown from 'react-bootstrap/NavDropdown';
-
-// function CollapsibleExample() {
-//   return (
-//     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-//       <Container>
-//         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//         <Navbar.Collapse id="responsive-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link href="#features">Features</Nav.Link>
-//             <Nav.Link href="#pricing">Pricing</Nav.Link>
-//             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-//               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.2">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action/3.4">
-//                 Separated link
-//               </NavDropdown.Item>
-//             </NavDropdown>
-//           </Nav>
-//           <Nav>
-//             <Nav.Link href="#deets">More deets</Nav.Link>
-//             <Nav.Link eventKey={2} href="#memes">
-//               Dank memes
-//             </Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
-
-// export default CollapsibleExample;
 const Layout = () => {
   return (
     <>
@@ -62,19 +22,3 @@ const Layout = () => {
 };
 
 export default Layout;
-
-{
-  /* <nav class="navbar navbar-default">
-<div class="container-fluid">
-  <div class="navbar-header">
-    <a class="navbar-brand" href="#">WebSiteName</a>
-  </div>
-  <ul class="nav navbar-nav">
-    <li class="active"><a href="#">Home</a></li>
-    <li><a href="#">Page 1</a></li>
-    <li><a href="#">Page 2</a></li>
-    <li><a href="#">Page 3</a></li>
-  </ul>
-</div>
-</nav> */
-}
