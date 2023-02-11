a3083c09a0cd562fb2aa1d8d328c2690db0bb1ab
Henil Panchal <henilp105@gmail.com>
2023-02-11 17:40:31 +0530

    user page

--- registry/src/pages/user.js
+++ registry/src/pages/user.js
@@ -2,8 +2,11 @@ import React from "react";
 import { MDBIcon } from "mdbreact";
 import { useNavigate, useParams } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import Figure from "react-bootstrap/Figure";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./upload.css";
 
 const UserPage = () => {
@@ -12,46 +15,96 @@ const UserPage = () => {
   const [email, setEmail] = React.useState("");
   const [dateJoined, setDateJoined] = React.useState("");
   const [projects, setProjects] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   const url = `${process.env.REACT_APP_REGISTRY_API_URL}/users/${user}`;
-  fetch(url)
-    .then((res) => {
-      if (res.ok) {
-        console.log("success");
-        return res.json();
-      } else {
-        console.error("Error while sending request");
-        navigate("/404");
-      }
-    })
-    .then((data) => {
-      setDateJoined(data["user"].date_joined);
-      setProjects(data["user"].packages);
-      setEmail(data["user"].email);
-      console.log(data);
-    });
+
+  if (isLoading) {
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          console.log("success");
+          return res.json();
+        } else {
+          console.error("Error while sending request");
+          setIsLoading(false);
+          navigate("/404");
+        }
+      })
+      .then((data) => {
+        setDateJoined(data["user"].createdAt);
+        setProjects(data["user"].packages);
+        setEmail(data["user"].email);
+        console.log(data);
+        setIsLoading(false);
+      });
+  }
 
   return (
     <Container>
-      <Card className="user-card" style={{ width: '18rem' }}>
-        <Card.Img 
-          variant="top" 
-          src={`https://www.gravatar.com/avatar/${user}`}
-          alt={`Avatar for ${user} from gravatar.com`}
-          title={`Avatar for ${user} from gravatar.com`}
-        />
-        <Card.Body>
-          <Card.Title>
-            <MDBIcon fas icon="user-circle" />
-            {user}
-          </Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
+      <Row>
+        <Col sm={4}>
+          <Row style={{ marginLeft: "10px", marginTop: "20px" }}>
+            <Figure>
+              <Figure.Image
+                width={171}
+                height={180}
+                alt={`Avatar for ${user} from gravatar.com`}
+                src={`https://www.gravatar.com/avatar/${user}`}
+              />
+            </Figure>
+          </Row>
+          <Row
+            style={{ marginLeft: "10px", marginTop: "10px", fontSize: "20px" }}
+          >
+            <MDBIcon style={{ marginTop: "5px" }} far icon="user-circle">
+              {" " + user}
+            </MDBIcon>
+            <MDBIcon style={{ marginTop: "5px" }} far icon="calendar-alt">
+              {" Joined " + Date(dateJoined).slice(4, 15)}
+            </MDBIcon>
+
+            <MDBIcon style={{ marginTop: "5px" }} far icon="envelope">
+              {" " + email}
+            </MDBIcon>
+          </Row>
+        </Col>
+        <Col sm={8}>
+          <Row style={{ fontSize: 24, marginTop: "20px" }}>
+            {projects.length === 0
+              ? "0 projects"
+              : projects.length + " projects"}
+          </Row>
+          {projects.map((element, index) => (
+            <Row
+              className="mb-2"
+              key={element.name + element.namespace_name}
+              xs={8}
+              md={8}
+            >
+              <Card>
+                {" "}
+                <Card.Link
+                  style={{ textDecoration: "none" }}
+                  href={`/packages/${element.namespace_name}/${element.name}`}
+                >
+                  <Card.Body>
+                    <Card.Title>{element.name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {element.namespace_name} / {element.name}{" "}
+                    </Card.Subtitle>
+
+                    <Card.Text id="card-text">
+                      <div>Last released {element.updatedAt}</div>
+                      <div> {element.description.slice(0, 40)}</div>
+                    </Card.Text>
+                  </Card.Body>
+                </Card.Link>
+              </Card>
+            </Row>
+          ))}
+        </Col>
+      </Row>
     </Container>
   );
 };
