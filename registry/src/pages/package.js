559d70c85f86c52191f58ffb296068dcdf56842f
Henil Panchal <henilp105@gmail.com>
2023-02-12 15:37:07 +0530

    NavBar Clean up and Package Page

---
+++ registry/src/pages/package.js
@@ -0,0 +1,118 @@
+import React from "react";
+import { MDBIcon } from "mdbreact";
+import { useNavigate, useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+import Figure from "react-bootstrap/Figure";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./upload.css";
+
+const PackagePage = () => {
+  const { package_name,namespace_name } = useParams();
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState("");
+  const [dateJoined, setDateJoined] = React.useState("");
+  const [projects, setProjects] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  const url = `${process.env.REACT_APP_REGISTRY_API_URL}/packages/${namespace_name}/${package_name}`;
+
+  if (isLoading) {
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          console.log("success");
+          return res.json();
+        } else {
+          console.error("Error while sending request");
+          setIsLoading(false);
+          navigate("/404");
+        }
+      })
+      .then((data) => {
+        // setDateJoined(data["user"].createdAt);
+        // setProjects(data["user"].packages);
+        // setEmail(data["user"].email);
+        
+     {/*  "name": package["name"],
+            "namespace": namespace["namespace"],
+            "latest_version_data": package["versions"][-1],
+            "author": package_author["name"],
+            "tags": package["tags"],
+            "license": package["license"],
+            "createdAt": package["createdAt"],
+            "version_history": package["versions"],
+            "updatedAt": package["updatedAt"],
+      "description": package["description"], */}
+        console.log(data);
+        setIsLoading(false);
+      });
+  }
+
+  return (
+    <Container>
+      <Row  style={{ marginLeft: "10px", marginTop: "20px" }}>
+        {{package_name}}
+     
+        <Col sm={4}>
+          <Row style={{ marginLeft: "10px", marginTop: "20px" }}>
+            hi
+          </Row>
+           <Row
+            style={{ marginLeft: "10px", marginTop: "10px", fontSize: "20px" }}
+          >
+            {/* <MDBIcon style={{ marginTop: "5px" }} far icon="user-circle">
+              {" " + user}
+            </MDBIcon>
+            <MDBIcon style={{ marginTop: "5px" }} far icon="calendar-alt">
+              {" Joined " + Date(dateJoined).slice(4, 15)}
+            </MDBIcon>
+
+            <MDBIcon style={{ marginTop: "5px" }} far icon="envelope">
+              {" " + email}
+            </MDBIcon>  */}
+          </Row>
+        </Col>
+        <Col sm={8}>
+          <Row style={{ fontSize: 24, marginTop: "20px" }}>
+            {projects.length === 0
+              ? "0 projects"
+              : projects.length + " projects"}
+          </Row>
+          {projects.map((element, index) => (
+            <Row
+              className="mb-2"
+              key={element.name + element.namespace_name}
+              xs={8}
+              md={8}
+            >
+              <Card>
+                {" "}
+                <Card.Link
+                  style={{ textDecoration: "none" }}
+                  href={`/packages/${element.namespace_name}/${element.name}`}
+                >
+                  <Card.Body>
+                    <Card.Title>{element.name}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      {element.namespace_name} / {element.name}{" "}
+                    </Card.Subtitle>
+
+                    <Card.Text id="card-text">
+                      <div>Last released {element.updatedAt}</div>
+                      <div> {element.description.slice(0, 40)}</div>
+                    </Card.Text>
+                  </Card.Body>
+                </Card.Link>
+              </Card>
+            </Row>
+          ))}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default PackagePage;
