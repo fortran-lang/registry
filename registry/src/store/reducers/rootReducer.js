7d2cfa3dbecc0ec9d030840a4da2ef721c8401ce
Henil Panchal <henilp105@gmail.com>
2023-02-12 15:14:33 +0530

    user redux page

--- registry/src/store/reducers/rootReducer.js
+++ registry/src/store/reducers/rootReducer.js
@@ -1,12 +1,14 @@
 import authReducer from "./authReducer";
 import accountReducer from "./accountReducer";
 import dashboardReducer from "./dashboardReducer";
+import userReducer from "./userReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   dashboard: dashboardReducer,
   account: accountReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
