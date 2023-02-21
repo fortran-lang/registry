15926de808324a99b02a5e5632f5ee6eda5036ab
arteevraina <arteevraina@gmail.com>
2023-02-21 13:05:27 +0530

    feat: show formatted date in the Package Item List Tile

--- registry/src/components/packageItem.js
+++ registry/src/components/packageItem.js
@@ -2,6 +2,36 @@ import { MDBListGroupItem } from "mdb-react-ui-kit";
 import { Row, Col, Image } from "react-bootstrap";
 
 const PackageItem = ({ packageEntity }) => {
+  function formatDate(timestamp) {
+    const now = new Date();
+    const date = new Date(timestamp);
+
+    // Check if the date is less than 12 hours ago
+    const diff = (now.getTime() - date.getTime()) / 1000;
+    if (diff < 43200) {
+      // 12 hours = 43,200 seconds
+      // Calculate minutes or hours ago
+      const minutes = Math.floor(diff / 60);
+      if (minutes < 60) {
+        return `${minutes} minutes ago`;
+      } else {
+        const hours = Math.floor(minutes / 60);
+        return `${hours} hours ago`;
+      }
+    } else {
+      // Format date in "YYYY-MM-DD" format
+      const year = date.getUTCFullYear();
+      const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+      const monthName = new Date(Date.UTC(year, month, 1)).toLocaleString(
+        "en-US",
+        { month: "long" }
+      );
+      const day = String(date.getUTCDate()).padStart(2, "0");
+
+      return `${monthName} ${day},  ${year}`;
+    }
+  }
+
   return (
     <MDBListGroupItem id="list-item">
       <Row>
@@ -17,19 +47,14 @@ const PackageItem = ({ packageEntity }) => {
           <div>
             <h5 id="list-item-package-name">{packageEntity.name}</h5>
           </div>
-          <h6 className="mb-2 text-muted">{packageEntity.description}</h6>
           <h6 className="mb-2 text-muted">
             Namespace {packageEntity.namespace}
           </h6>
+          <label className="mb-2 text-muted">{packageEntity.description}</label>
         </Col>
-        <Col md={1} style={{ flex: 1 }}>
-          <h6
-            style={{
-              textAlign: "right",
-            }}
-          >
-            By {packageEntity.author}
-          </h6>
+        <Col md={1} style={{ flex: 1, textAlign: "right" }}>
+          <h6>By {packageEntity.author}</h6>
+          <label>{formatDate(packageEntity.updatedAt)}</label>
         </Col>
       </Row>
     </MDBListGroupItem>
