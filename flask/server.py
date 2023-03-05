dd710840b2d2c43cc240e0eb89981f12973414cf
Henil Panchal <henilp105@gmail.com>
2023-03-05 23:35:43 +0530

    Refactor: remove redundant code and clean code

--- flask/server.py
+++ flask/server.py
@@ -5,38 +5,11 @@ from mongo import db
 import auth
 import user
 import packages
+import namespaces
 
 @app.route("/")
 def index():
     return jsonify({"message": "Python flask mongo", "code": 200})
-
-@app.route("/account/login", methods=["GET"])
-def login_UI():
-    return render_template("login.html")
-
-@app.route("/search", methods=["GET"])
-def search():
-    return render_template("search.html")
-
-@app.route("/account/register", methods=["GET"])
-def register_UI():
-    return render_template("signup.html")
-
-@app.route("/account/reset-password", methods=["GET"])
-def render_reset():
-    return render_template("reset.html")
-
-@app.route("/account/update-password", methods=["GET"])
-def update_password():
-    return render_template("update.html")
-
-@app.route("/manage/projects/", methods=["GET"])
-def manage_projects():
-    return render_template("account.html")
-
-@app.route("/manage/account/", methods=["GET"])
-def manage_accounts():
-    return render_template("account.html")
 
 @app.errorhandler(404)
 def page_not_found(e):
