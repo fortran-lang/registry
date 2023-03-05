d497437882d8a583abc11e4d637c55ed43e415e1
Henil Panchal <henilp105@gmail.com>
2023-03-05 23:34:11 +0530

    Refactor: namespaces

---
+++ flask/namespaces.py
@@ -0,0 +1,73 @@
+from app import app
+from mongo import db
+from flask import request, jsonify
+from app import swagger
+from flasgger.utils import swag_from
+
+@app.route("/packages/<namespace_name>/delete", methods=["POST"])
+def delete_namespace(namespace_name):
+    uuid = request.form.get("uuid")
+
+    if not uuid:
+        return jsonify({"status": "error", "message": "Unauthorized"}), 401
+
+    user = db.users.find_one({"uuid": uuid})
+
+    # Check if the user is authorized to delete the package.
+    if not "admin" in user["roles"]:
+        return (
+            jsonify(
+                {
+                    "status": "error",
+                    "message": "User is not authorized to delete the namespace",
+                }
+            ),
+            401,
+        )
+
+    # Get the namespace from the namespace_name.
+    namespace = db.namespaces.find_one({"namespace": namespace_name})
+
+    # If namespace is not found. Return 404.
+    if not namespace:
+        return jsonify({"message": "Namespace not found", "code": 404})
+
+    namespace_deleted = db.namespaces.delete_one({"namespace": namespace["_id"]})
+
+    if namespace_deleted.deleted_count > 0:
+        return jsonify({"message": "Namespace deleted successfully"}), 200
+    else:
+        return jsonify({"message": "Internal Server Error", "code": 500})
+    
+
+@app.route("/namespace/<namespace>", methods=["GET"])
+def namespace_packages(namespace):
+    namespace_packages = db.namespaces.find_one({"namespace": namespace})
+    packages = []
+    for i in namespace_packages["packages"]:
+        package = db.packages.find(
+            {"_id": i},
+            {
+                "_id": 0,
+                "name": 1,
+                "description": 1,
+                "author": 1,
+                "updatedAt": 1,
+            },
+        )
+        for p in package:
+            p["namespace"] = namespace
+            author = db.users.find_one({"_id": p["author"]})
+            p["author"] = author["username"]
+            packages.append(p)
+
+    return (
+        jsonify(
+            {
+                "status": 200,
+                "packages": packages,
+                "createdAt": namespace_packages["createdAt"],
+            }
+        ),
+        200,
+    )
