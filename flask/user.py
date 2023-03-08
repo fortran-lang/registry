f158a228f61f877ced6c23b84ec1078c74be017d
arteevraina <arteevraina@gmail.com>
2023-03-08 22:39:39 +0530

    feat: maintainers can assign users to packages

--- flask/user.py
+++ flask/user.py
@@ -7,6 +7,7 @@ from flask import request, jsonify
 from app import swagger
 from flasgger.utils import swag_from
 from auth import forgot_password
+from packages import checkUserUnauthorized
 
 load_dotenv()
 
@@ -183,13 +184,26 @@ def transfer_account():
             200,
         )
 
-@app.route("/<username>/<namespace>/maintainer", methods=["POST"])
-def add_maintainers(namespace, username):
-    uuid = request.get_json()["uuid"]
-    username_to_be_added = request.get_json()["username"]
+@app.route("/<username>/maintainer", methods=["POST"])
+def add_maintainers(username):
+    uuid = request.form.get("uuid")
+    username_to_be_added = request.form.get("username")
+    package = request.form.get("package")
+    namespace = request.form.get("namespace")
+
+    # Validating the data coming with request.
     if not uuid:
         return jsonify({"message": "Unauthorized", "code": 401}), 401
-
+    
+    if not username_to_be_added:
+        return jsonify({"message": "Please enter the username to be added", "code": 400}), 400
+    
+    if not package:
+        return jsonify({"message": "Please enter the package name", "code": 400}), 400
+    
+    if not namespace:
+        return jsonify({"message": "Please enter the namespace name", "code": 400}), 400
+    
     # Get the user from the database using uuid.
     user = db.users.find_one({"uuid": uuid})
 
@@ -203,31 +217,37 @@ def add_maintainers(namespace, username):
     # Check if namespace does not exists.
     if not package_namespace:
         return jsonify({"message": "Namespace not found", "code": 404}), 404
+    
+    # Get the package in the namespace.
+    curr_package = db.packages.find_one({"name": package, "namespace": package_namespace["_id"]})
+
+    # Check if the package does not exists.
+    if not curr_package:
+        return jsonify({"message": "Package not found", "code": 404}), 404
 
     # Check if the current user has authority to add maintainers.
-    if checkUserNotAdmin(user_id=user["_id"], namespace=package_namespace):
+    if checkUserUnauthorized(user_id=user["_id"], package_namespace=package_namespace):
         return jsonify({"message": "Unauthorized", "code": 401}), 401
     
-
     # Get the user to be added using the username received in the request body.
     user_to_be_added = db.users.find_one({"username": username_to_be_added})
 
     if not user_to_be_added:
         return jsonify({"message": "Username to be added as a maintainer not found", "code": 404})
 
-     # Update the document only if the user_to_be_added["_id"] is not already in the admins list.
-    result =  db.namespaces.update_one(
-        {"namespace": namespace, 'maintainers': {'$ne': user_to_be_added["_id"]}}, 
+    # Update the document only if the user_to_be_added["_id"] is not already in the maintainers list.
+    result = db.packages.update_one(
+        {"name": package, "namespace": package_namespace["_id"]},
         {"$addToSet": {"maintainers": user_to_be_added["_id"]}}
+    )
+
+    # package_id gets added to the maintainerOf list in user document.
+    db.users.update_one(
+        {"username": username_to_be_added}, 
+        {"$addToSet": {"maintainerOf": curr_package["_id"]}}
     )
 
     if result.modified_count > 0:
         return jsonify({"message": "Maintainer added successfully", "code": 200}), 200
     else:
         return jsonify({"message": "Maintainer already added", "code": 200}), 200
-
-def checkUserNotAdmin(user_id, namespace):
-    user_id_str = str(user_id)
-    admin_id_list = [str(obj_id) for obj_id in namespace["admins"]]
-
-    return user_id_str not in admin_id_list
