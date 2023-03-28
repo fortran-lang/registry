457dca298f799da7e27e060101e976003bc5c871
Henil Panchal <henilp105@gmail.com>
2023-03-28 18:29:01 +0530

    feat: fix mongo connections for test cases

--- tests/base_case.py
+++ tests/base_case.py
@@ -1,6 +1,22 @@
 import unittest
-from mongo import client
-from server import app
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from gridfs import GridFS
+from flask import Flask
+
+app = Flask(__name__)
+load_dotenv()
+
+database_name = os.environ['MONGO_DB_NAME']
+try:
+    mongo_uri = os.environ['MONGO_URI']
+    client = MongoClient(mongo_uri)
+except KeyError as err:
+    print("Add MONGO_URI to .env file")
+
+db = client[database_name]
+file_storage = GridFS(db, collection="tarballs")
 
 class BaseTestClass(unittest.TestCase):
     def setUp(self):
