a810de7d16c145877997028c91fcc311f8935464
arteevraina <arteevraina@gmail.com>
2023-04-06 10:20:56 +0530

    test: added test for checking incorrect/prev version upload

--- tests/test_packages.py
+++ tests/test_packages.py
@@ -86,3 +86,38 @@ class TestPackages(BaseTestClass):
 
         response = self.client.post("/packages", data=TestPackages.test_package_data)
         self.assertEqual(400, response.json["code"])
+
+    def test_incorrect_version_upload(self):
+        """
+        Test case to verify the behaviour of the system when a user tries to upload a package with
+        incorrect version.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+
+        Raises:
+        AssertionError: If the response received from the server is not as expected.
+        """
+
+        response = self.client.post("/auth/signup", data=TestPackages.test_user_data)
+        self.assertEqual(200, response.json["code"])
+
+        uuid = response.json["uuid"]
+
+        TestPackages.test_package_data["uuid"] = uuid
+        TestPackages.test_package_data["tarball"] = TestPackages.generate_tarball()
+
+        response = self.client.post("/packages", data=TestPackages.test_package_data)
+        self.assertEqual(200, response.json["code"])
+
+        TestPackages.test_package_data["tarball"] = TestPackages.generate_tarball()
+
+        # Make the previous one previous to the correct one.
+        TestPackages.test_package_data["version"] = "0.0.0"
+
+        response = self.client.post("/packages", data=TestPackages.test_package_data)
+
+        self.assertEqual(400, response.json["code"])
