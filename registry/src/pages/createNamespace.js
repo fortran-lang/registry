7765e99c64763cebd0e897d55d34efed3b1e470b
Henil Panchal <henilp105@gmail.com>
2023-04-08 20:15:50 +0530

    feat : UI for create namespace

--- registry/src/pages/createNamespace.js
+++ registry/src/pages/createNamespace.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
-import { Navigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { createNamespace } from "../store/actions/createNamespaceActions";
 import Container from "react-bootstrap/Container";
@@ -10,9 +9,9 @@ import "./upload.css";
 const NamespaceForm = () => {
   const [cookies, setCookie] = useCookies(["uuid"]);
   const dispatch = useDispatch();
-  const isLoading = useSelector((state) => state.upload.isLoading);
-  const message = useSelector((state) => state.upload.message);
-  const statuscode = useSelector((state) => state.upload.statuscode);
+  const isLoading = useSelector((state) => state.createNamespace.isLoading);
+  const message = useSelector((state) => state.createNamespace.message);
+  const statuscode = useSelector((state) => state.createNamespace.statuscode);
 
   const [data, setData] = useState({
     namespace: "",
@@ -30,52 +29,52 @@ const NamespaceForm = () => {
     dispatch(createNamespace(event.target));
   };
 
+  return !isLoading ? (
+    <Container style={{ padding: 20 }}>
+      <h2>Create a Namespace</h2>
+      <form id="package-form" onSubmit={handleSubmit}>
+        <label htmlFor="namespace">Namespace:</label>
+        <input
+          type="text"
+          id="namespace"
+          name="namespace"
+          value={data.namespace}
+          onChange={handleChange}
+        />
+        <br />
 
-    return !isLoading ? (
-        <Container style={{padding:20}}>
-        <h2>Create a Namespace</h2>
-        <form id="package-form" onSubmit={handleSubmit}>
-          <label htmlFor="namespace">Namespace:</label>
-          <input
-            type="text"
-            id="namespace"
-            name="namespace"
-            value={data.namespace}
-            onChange={handleChange}
-          />
-          <br />
-      
-          <label htmlFor="namespace_description">Namespace Description:</label>
-          <textarea
-            id="namespace_description"
-            name="namespace_description"
-            value={data.namespace_description}
-            onChange={handleChange}
-          />
-          <br />
-      
-          <input
-            type="hidden"
-            id="uuid"
-            name="uuid"
-            value={cookies.uuid}
-            onChange={handleChange}
-          />
-          <br />
-          {statuscode === 200 ? (
-            <Container className="success">{message}</Container>
-          ) : (
-            <Container className="error">{message}</Container>
-          )}
-          <button type="submit">Create Namespace</button>
-        </form>
-</Container>    ) : (
-      <Container style={{ margin: "200px" }}>
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </Spinner>
-      </Container>
-    );
-  }
+        <label htmlFor="namespace_description">Namespace Description:</label>
+        <textarea
+          id="namespace_description"
+          name="namespace_description"
+          value={data.namespace_description}
+          onChange={handleChange}
+        />
+        <br />
+
+        <input
+          type="hidden"
+          id="uuid"
+          name="uuid"
+          value={cookies.uuid}
+          onChange={handleChange}
+        />
+        <br />
+        {statuscode === 200 ? (
+          <Container className="success">{message}</Container>
+        ) : (
+          <Container className="error">{message}</Container>
+        )}
+        <button type="submit">Create Namespace</button>
+      </form>
+    </Container>
+  ) : (
+    <Container style={{ margin: "200px" }}>
+      <Spinner animation="border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    </Container>
+  );
+};
 
 export default NamespaceForm;
