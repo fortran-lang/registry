6ff29076c6d44522589baf5ab67ed7130bb9dc79
arteevraina <arteevraina@gmail.com>
2023-05-28 13:15:07 +0530

    feat: added the actions & reducers

--- registry/src/store/actions/packageActions.js
+++ registry/src/store/actions/packageActions.js
@@ -2,6 +2,9 @@ import axios from "axios";
 export const FETCH_PACKAGE_DATA = "FETCH_PACKAGE_DATA";
 export const FETCH_PACKAGE_DATA_SUCCESS = "FETCH_PACKAGE_DATA_SUCCESS";
 export const FETCH_PACKAGE_DATA_ERROR = "FETCH_PACKAGE_DATA_ERROR";
+export const VERIFY_USER_ROLE = "VERIFY_USER_ROLE";
+export const VERIFY_USER_ROLE_SUCCESS = "VERIFY_USER_ROLE_SUCCESS";
+export const VERIFY_USER_ROLE_ERROR = "VERIFY_USER_ROLE_ERROR";
 
 export const fetchPackageData =
   (namespace_name, package_name) => async (dispatch) => {
@@ -29,6 +32,38 @@ export const fetchPackageData =
         payload: {
           statuscode: error.data.code,
           data: error.data.data,
+        },
+      });
+    }
+  };
+
+export const verifyUserRole =
+  (namespace_name, package_name, uuid) => async (dispatch) => {
+    const formData = new FormData();
+    formData.append("uuid", uuid);
+
+    dispatch({
+      type: VERIFY_USER_ROLE,
+    });
+
+    try {
+      let result = await axios({
+        method: "post",
+        url: `${process.env.REACT_APP_REGISTRY_API_URL}/packages/${namespace_name}/${package_name}/verify`,
+        data: formData,
+      });
+
+      dispatch({
+        type: VERIFY_USER_ROLE_SUCCESS,
+        payload: {
+          data: result.data,
+        },
+      });
+    } catch (error) {
+      dispatch({
+        type: VERIFY_USER_ROLE_ERROR,
+        payload: {
+          data: error.data,
         },
       });
     }
--- registry/src/store/reducers/packageReducer.js
+++ registry/src/store/reducers/packageReducer.js
@@ -2,12 +2,16 @@ import {
   FETCH_PACKAGE_DATA,
   FETCH_PACKAGE_DATA_SUCCESS,
   FETCH_PACKAGE_DATA_ERROR,
+  VERIFY_USER_ROLE_SUCCESS,
+  VERIFY_USER_ROLE_ERROR,
+  VERIFY_USER_ROLE,
 } from "../actions/packageActions";
 
 const initialState = {
   statuscode: 0,
   data: [],
   isLoading: true,
+  isVerified: null,
 };
 
 const packageReducer = (state = initialState, action) => {
@@ -30,6 +34,21 @@ const packageReducer = (state = initialState, action) => {
         isLoading: false,
         statuscode: action.payload.statuscode,
         data: action.payload.data,
+      };
+    case VERIFY_USER_ROLE_SUCCESS:
+      return {
+        ...state,
+        isVerified: action.payload.data.isVerified,
+      };
+    case VERIFY_USER_ROLE_ERROR:
+      return {
+        ...state,
+        isVerified: false,
+      };
+    case VERIFY_USER_ROLE:
+      return {
+        ...state,
+        isVerified: null,
       };
     default:
       return state;
