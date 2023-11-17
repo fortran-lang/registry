ee885f425f4c3be91d2e9f43a262cb40129b0e95
Henil Panchal <henilp105@gmail.com>
2023-11-17 20:00:17 +0530

    fix: package validation

--- backend/validate_package.py
+++ backend/validate_package.py
@@ -1,28 +1,28 @@
 import os
 import docker
 from app import app
+import toml
 
 # Package testing container
 client = docker.from_env()
 container = client.containers.run(
-    "registry",
+    "validate_package",
     tty=True,
     detach=True,
     network_disabled=False
 )
 
 
-def validate_package(package, packagename):
+def validate(package, packagename):
     data = package.read()
     container.put_archive(os.path.dirname(f"/home/registry/{packagename}.tar.gz"), data)
     build_response = container.exec_run(f'sh -c "cd /home/registry/{packagename} &&  /home/registry/fpm build"')
+    toml_data = container.exec_run(f'sh -c "cd /home/registry/{packagename} && cat fpm.toml"')
+    parsed_toml = toml.loads(str(toml_data, 'utf-8'))
     container.exec_run(f'sh -c "cd /home/registry/ && rm -rf {packagename}"')
     if b'<ERROR>' in build_response.output:
         # Package build failed 
-        # print("build failed")
-        return False
+        return False, None
     if b'[100%] Project compiled successfully.' in build_response.output:
         # Package build success 
-        # print("build success")
-        # print(build_response.output)
-        return True
+        return True, parsed_toml
