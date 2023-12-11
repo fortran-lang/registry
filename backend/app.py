356191688dcd3a6d206926a1a2c1e36e48db7582
arteevraina <arteevraina@gmail.com>
2023-12-11 11:45:20 +0530

    dep: setup structure for adding jwt auth

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
 from flasgger import Swagger
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
+JWTManager(app)
 
 swagger = Swagger(
     app,
