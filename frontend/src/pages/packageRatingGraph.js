ea30b299c3a093a5597f2d4b330e334406a7cb2c
arteevraina <arteevraina@gmail.com>
2024-02-17 14:53:39 +0530

    fix: fallback condition for ui if no ratings available

--- frontend/src/pages/packageRatingGraph.js
+++ frontend/src/pages/packageRatingGraph.js
@@ -9,7 +9,12 @@ const PackageRatingGraph = ({ data }) => {
     star: value,
   }));
 
-  return (
+  return parsedArray.length === 0 ? (
+    <div>
+      No stats available right now. This will update as soon as the package gets
+      rated.
+    </div>
+  ) : (
     <BarChart width={600} height={500} data={parsedArray}>
       <XAxis dataKey="name" />
       <Bar dataKey="star" fill="#8884d8" />
