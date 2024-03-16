c84244eba8f897ce43bee2b1bacb514a1efd0e28
Henil Panchal <henilp105@gmail.com>
2024-03-16 13:20:15 +0530

    add: package validation , digests hash generation and  checking and resolves: #65

--- backend/validate.py
+++ backend/validate.py
@@ -6,6 +6,7 @@ from mongo import file_storage
 from bson.objectid import ObjectId
 from gridfs.errors import NoFile
 import toml
+from check_digests import check_digests
 
 
 def run_command(command):
@@ -15,11 +16,16 @@ def run_command(command):
         print(result.stderr)
     return result.stdout if result.stdout else result.stderr
 
+def collect_dependencies(section, parsed_toml):
+    dependencies = list()
+    for dependency_dict in parsed_toml.get(section, []):
+        for dependency_name, dependency_info in dependency_dict.get('dependencies', {}).items():
+            dependencies.append((dependency_info['namespace'],dependency_name))
+    return dependencies
+
 def process_package(packagename):
-    # Create a temp directory
     create_dir_command = f'mkdir -p static/temp/{packagename}'
     run_command(create_dir_command)
-    # Extract the archive
     extract_command = f'tar -xzf static/temp/{packagename}.tar.gz -C static/temp/{packagename}/'
     run_command(extract_command)
 
@@ -30,18 +36,20 @@ def process_package(packagename):
             file_content = file.read()
         parsed_toml = toml.loads(file_content) # handle toml parsing errors
     except:
-        return False, None
+        return False, None,"Error parsing toml file"
+    
+    result = check_digests(f'static/temp/{packagename}/')
 
     # Clean up
     cleanup_command = f'rm -rf static/temp/{packagename} static/temp/{packagename}.tar.gz'
     run_command(cleanup_command)
 
-    # if '<ERROR>' in result:
-    #     # Package build failed 
-    #     return False, None
-    # if '[100%] Project compiled successfully.' in result:
-    #     # Package build success 
-    #     return True, parsed_toml
+    if result[0]!= 0:
+        # Package verification failed 
+        return False, parsed_toml, "Digests do not match"
+    if '[100%] Project compiled successfully.' in result:
+        # Package verification success 
+        return True, parsed_toml, "Package verified successfully."
 
 
 def validate():
@@ -50,61 +58,95 @@ def validate():
     for  package in packages:
         for i in package['versions']:
             if 'isVerified' in i.keys() and i['isVerified'] == False:
-                tarball = file_storage.get(ObjectId(i['oid']))
+                try:
+                    tarball = file_storage.get(ObjectId(i['oid']))
+                except NoFile:
+                    print("No tarball found for " + package['name'] + " " + i['version'])
+                    continue
                 packagename = package['name'] + '-' + i['version']
                 with open(f"static/temp/{packagename}.tar.gz", "wb") as f:
                     f.write(tarball.read())
                 result = process_package(packagename)
-                
+                update_data = {}
                 if result[0] == False:
-                    db.packages.update_one({"name": packages['name'],"namespace":package['namespace']}, {"$set": {"versions.$[elem].unabletoVerify": True}}, array_filters=[{"elem.version": i['version']}])
+                    update_data['isVerified'] = False
+                    update_data['unabletoVerify'] = True
                     print("Package tests failed for " + packagename)
-                    continue
                 else:
                     print("Package tests success for " + packagename)
-                    db.packages.update_one({"name": package['name'],"namespace":package['namespace']}, {"$set": {"versions.$[elem].isVerified": True}}, array_filters=[{"elem.version": i['version']}])
+                    update_data['isVerified'] = True
 
-                update_data = {}
+                if result[2] == "Error parsing toml file":
+                    db.packages.update_one({"name": package['name'],"namespace":package['namespace']}, {"$set": update_data})
+                    pass
 
-                for key in ['repository', 'copyright', 'description',"homepage"]:
-                    if key in result[1] and package[key] == "Package Under Verification":
-                        update_data[key] = result[1][key]
+                # if result[0] == False:
+                #     db.packages.update_one({"name": packages['name'],"namespace":package['namespace']}, {"$set": {"versions.$[elem].unabletoVerify": True}}, array_filters=[{"elem.version": i['version']}])
+                #     print("Package tests failed for " + packagename)
+                # else:
+                #     print("Package tests success for " + packagename)
+                #     db.packages.update_one({"name": package['name'],"namespace":package['namespace']}, {"$set": {"versions.$[elem].isVerified": True}}, array_filters=[{"elem.version": i['version']}])
+                     
 
-                for key in ['categories', 'keywords']:
-                    if key in result[1]:
-                        update_data[key] = package[key] + result[1][key]
+                for key in ['repository', 'copyright', 'description',"homepage", 'categories', 'keywords']:
+                    if key in result[1] and package[key] != result[1][key]:
+                        if key in ['categories', 'keywords']:
+                            update_data[key] = package[key] + result[1][key]
+                        else:
+                            update_data[key] = result[1][key]
+
+                dependencies = list()
+                dependencies += [(dependency_info['namespace'],dependency_name) for dependency_name, dependency_info in result[1].get('dependencies', {}).items()]
+                for section in ['test', 'example', 'executable']:
+                    dependencies += collect_dependencies(section, result[1])
+                
+                # TODO: verify if the dependencies are present in the database
+                update_data['dependencies'] = list(set(dependencies))
+
+                for i in dependencies:
+                    dependency_package = db.packages.find_one({"name": i[1], "namespace": i[0]}) # also verify the version
+                    if dependency_package is None:
+                        print(f"Dependency {i[0]}/{i[1]} not found in the database")
+                        update_data['isVerified'] = False                     # if any dependency is not found, the package is not verified      
 
                 for k,v in package.items():
                     if v == "Package Under Verification" and k not in update_data.keys():
-                        update_data[k] = f"{k} key not provided."
+                        update_data[k] = f"{k} not provided."
 
                 db.packages.update_one({"name": package['name'],"namespace":package['namespace']}, {"$set": update_data})
-    return 0
+
 
 validate()
 
 
-# target checking of sections:
+# # target checking of sections:
+# # upload time
+# # name: The name of the project 
+# # version: The version of the project
+# # license: The project license
 
-# upload time
-# name: The name of the project 
-# version: The version of the project
-# license: The project license
+# maintainer: Maintainer of the project                        auto supported by registry
+# author: Author of the project                                auto supported by registry
 
-maintainer: Maintainer of the project                        auto supported by registry
-author: Author of the project                                auto supported by registry
-
-repository: The project’s repository                         supported 
-homepage: The project’s homepage                             supported 
-description: Description of the project                      supported 
-copyright: Copyright of the project                          supported 
-keywords: Keywords describing the project                    supported 
-categories: Categories associated with the project           not supported
+# repository: The project’s repository                         supported 
+# homepage: The project’s homepage                             supported 
+# description: Description of the project                      supported 
+# copyright: Copyright of the project                          supported 
+# keywords: Keywords describing the project                    supported 
+# categories: Categories associated with the project           not supported
 
 
-# support categories and keywords
-# support the keywords and update from verification
-# fix 595 line  and packages model tages to keywords.
-# add categories to packages model 
+# # support categories and keywords
+# # support the keywords and update from verification
+# # fix 595 line  and packages model tages to keywords.
+# # add categories to packages model 
+# # package is verified if and only if all versions are verified
 
-# package is verified iff all versions are verified
+
+# firstly check verified of a package
+# if not verified then check for each version
+# if not verified then process the package
+# if verified then update the package
+# if not verified then update the package
+
+# bug fix package rating handling.
